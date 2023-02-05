347e076ef4e2b5fcef51322be6767e1781456f3a
Chet Gray <chetgray@gmail.com>
2023-02-04 21:24:56 -0500

    📝 docs: reword and rewrap docstrings

--- program1.py
+++ program1.py
@@ -1,86 +1,16 @@
 #!/usr/bin/env python3.9
 """CIT 244: Python II (4226_55Z1) - Program 1. Classes
 
-We're going to write a program that manages the email address of our
-contacts. Your program must contain a class named contact that has 3
-attributes (instance variables): last name, first name, and email. Your
-class should also have a method to return the full name and email
-address for printing when requested. You may add other methods to the
-class if you like.
+A program that manages the email address of our contacts. It contain a class named contact that
+has 3 attributes (instance variables): last name, first name, and email. The class also has a
+method to return the full name and email address for printing when requested.
 
-Each contact should use a single instance of your contact class. You
-should use either a list or a dictionary to act as a container of the
-contact instances.
+Each contact uses a single instance of the contact class. A list is used to act as a container
+of the contact instances.
 
-Also, the program should have a menu that allows the user to interact
-with your collection of contacts: displaying contacts or adding new
-contacts as long as the user wishes, then saying goodbye when finished.
-The menu should offer these options.
-
-.. code:: pycon
-
-   Program Options.
-      1.) Display all contacts
-      2.) Create new contact
-      3.) Exit
-   >>> option = input("Enter 1, 2, or 3: ")
-
-For full credit.
-
-- use a python class to represent the contact info and at least one
-  method to print that info.
-- collect each new contact instance in a list or dictionary.
-- please format printed output so all column data lines up.
-
-Here is a possible print-out, where we display some pre-existing
-contacts, add a new contact, then re-display the list to see that our
-contact has been added. Again, you are not required to save to disk
-before exiting.
-
-.. code:: console
-
-     Program Options.
-        1.) Display all contacts
-        2.) Create new contact
-        3.) Save and exit
-
-    Enter 1, 2, or 3: 1
-
-    Name            Email
-    smith, sue      ssmith@wer.com
-    Dean, hugh      hrdean@abc.com
-    green, sam      sgreen@rty.com
-
-     Program Options.
-        1.) Display all contacts
-        2.) Create new contact
-        3.) Save and exit
-
-    Enter 1, 2, or 3: 2
-
-    Enter contact's first name: mac
-    Enter contact's last name: davis
-    Enter contact email: mdavis@asd.com
-
-     Program Options.
-        1.) Display all contacts
-        2.) Create new contact
-        3.) Save and exit
-
-    Enter 1, 2, or 3: 1
-
-    Name            Email
-    smith, sue      ssmith@wer.com
-    Dean, hugh      hrdean@abc.com
-    green, sam      sgreen@rty.com
-    davis, mac      mdavis@asd.com
-
-     Program Options.
-        1.) Display all contacts
-        2.) Create new contact
-        3.) Save and exit
-
-    Enter 1, 2, or 3:
+The program has a menu that allows the user to interact with the collection of contacts:
+displaying contacts or adding new contacts as long as the user wishes, then saying goodbye when
+finished. The menu should offer these options.
 """
 
 __author__ = "Chet Gray <cgray0209@kctcs.edu>"
@@ -95,9 +25,9 @@ from typing import Iterable
 class Contact:
     """A contact has a last name, first name, and email address.
 
-    Your program must contain a class named contact that has 3 attributes (instance variables):
-    last name, first name, and email. Your class should also have a method to return the full
-    name and email address for printing when requested.
+    A class named contact that has 3 attributes (instance variables): last name, first name, and
+    email. It also has a method to return the full name and email address for printing when
+    requested.
     """
 
     def __init__(self, last_name: str, first_name: str, email: str) -> None:
@@ -170,9 +100,9 @@ def print_table(records: Iterable[object], columns: Iterable[Column]) -> None:
 def _main() -> None:
     """The main entry point of the program.
 
-    Also, the program should have a menu that allows the user to interact with your collection
-    of contacts: displaying contacts or adding new contacts as long as the user wishes, then
-    saying goodbye when finished. The menu should offer these options.
+    The program has a menu that allows the user to interact with the collection of contacts:
+    displaying contacts or adding new contacts as long as the user wishes, then saying goodbye
+    when finished.
     """
     contacts = [
         Contact("Marquardt", "Eda", "emarquardt@example.com"),
