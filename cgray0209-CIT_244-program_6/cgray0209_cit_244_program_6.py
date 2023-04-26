114b8b50af272962b5f728bc50c45c9df2fbe910
Chet Gray <chetgray@gmail.com>
2023-04-26 11:07:29 -0400

    🎉 build(program_6): begin new project

---
+++ cgray0209-CIT_244-program_6/cgray0209_cit_244_program_6.py
@@ -0,0 +1,7 @@
+"""CIT 244: Python II - Program 6: Bootstrap
+"""  # noqa: E501
+
+__author__ = "Chet Gray <cgray0209@kctcs.edu>"
+__copyright__ = "Copyright (c) 2023 Chet Gray"
+__license__ = "UNLICENSED"
+__version__ = "0.1.0"
